ea139e1fafe65abb271bfb017589fb94bc052abc
Saba Nozadze <nozadzesaba80@gmail.com>
2025-04-24 07:24:22 -0700

    commit

---
+++ day 01/classwork/cw.py
@@ -0,0 +1,9 @@
+#printi() funqcias viyenebt terminalshi chveni dawerili kodis shedegi rom gamovitanot
+
+city = "tbilisi"
+country = "Georgia"
+food = "xinkali"
+drink = "energy"
+print (city, country, food, drink )
+#printi () gamoaqvs shedegi teriminalshi. variable- inaxavs mnishvnelobas. int- mteli ricxvebi
+#booleans- True False. float- wiladi ricxvebi
---
+++ day 01/homework/cw.py
@@ -0,0 +1,23 @@
+#variables () es aris cvladi romelic gamoiyeneba raime saxelis shesanaxad
+# #-->es aris komentaris nishani romelic ignir-s aketebs kompiutershi da gvichans mxolod chven
+print("Saba")
+print(156)
+print(25.6)
+print(True)
+print(False)
+
+print("Gio")
+print (1080)
+print(109.7)
+
+print("Iura") 
+print(1200)
+print(101.9)
+
+print("Zuka")
+print(1660) 
+print(112.4)
+
+print("Tato")
+print(600) 
+print(13.6)
