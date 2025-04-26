54ecfc8cea958b9a402cb8b39bfe5c1f163486c4
Saba Nozadze <nozadzesaba80@gmail.com>
2025-04-26 03:41:55 -0700

    commit

---
+++ day 02/homework/cw.py
@@ -0,0 +1,45 @@
+#დეტალურად აღწერეთ თუ რა განსხვავებაა ბრჭყალებში ჩაწერილ რიცხვსა და ჩვეულებრივ ინტეჯერ რიცხვს შორის?
+# "15" aris stringi da ar aris  arit metika shesadzlebeli
+# 15 aris integeri ricxvi. chven shegvidzlia gamoviyenot aritmetikashi
+
+
+
+#დღეს ნასწავლ თითოეულ მათემატიკურ ოპერატორზე გააკეთეთ 5-5 მაგალითი სხვადასხვა რიცხვებით და თითოეულის შედეგი გამოიტანეთ ტერმინალში.
+num1 = 120
+num2 = 60
+num3 =15467
+num4 = 1237
+num5 = 3453
+ 
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+
+
+print (num3 + num4)
+print (num3 - num4)
+print (num3 * num4)
+print (num3 / num4)
+
+print  (num5 + num2)
+print  (num5 - num2)
+print  (num4 * num2)
+print  (num4 / num2)
+
+
+
+# შექმენით ორი ცვლადი და თითოეულში შეინახეთ რიცხვები. შექმენით ოთხი ცვლადი: 
+#• Addition
+#• Substraction
+#• Multiplication
+#• Division
+
+num6 = 300
+num7 = 150
+
+
+Addition =  num6 + num7
+Substraction = num6 - num7
+Multiplication = num6 * num7
+Division = num6 / num7
