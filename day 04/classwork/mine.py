35724d65b74bd90f3e6f37f9c534656d5f9f3049
Saba Nozadze <nozadzesaba80@gmail.com>
2025-05-01 10:02:10 -0700

    commit

---
+++ day 04/classwork/mine.py
@@ -0,0 +1,18 @@
+# print "lika" aq shecdomaa radgan print-tan ar weria frchxilebi da amoagdebs erors
+print ("lika")
+#analyst_salary =  3000
+#print(analyst_salary)
+analyst_salary = 3000
+print (Analyst_salary) #pirveli aso gansxvavdeba
+print (analyst_salary)
+
+n1=27
+n2='12' #brwyalebi uweria da stringad agiqvams
+print (n1/n2)
+n1=27
+n2=12
+
+
+Status = Mentor 
+print(status) #pirvel asoebshia sxvaoba
+print(Status)
---
+++ day 04/homework/mine.py
@@ -0,0 +1,27 @@
+name = "Giorgi"
+  #print(namme) #orjer weria m da arasworad agiqvavs
+print (name)
+
+
+
+
+# number = 5
+   #text = " apples"
+  # result = number + text
+   #print(result) #ar daiwereba radgan text  aris brwyalebshi shenaxuli
+number = "5"
+text = "apples"
+result = number + text
+print(result)
+
+#name = liKa
+   #name2 = naame + "4" #cvladishi weria zedmeti aso da name cvladshi mocemuli sityva ar aris brwyalebshi
+name = "lika"
+name2 = name + "4"
+
+
+obj = "books"
+print (obj + "pages")
+obj2 = obj + "page"
+print (obj)
+#es daprintavs bookspages tavidan da shemdeg mxolod books radgan printshi mititebulia cvladi obj
