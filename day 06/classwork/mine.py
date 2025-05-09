1aaafeb86ec5b2e04f2ff5dbf8a8e50bd0070d57
Saba Nozadze <nozadzesaba80@gmail.com>
2025-05-09 03:35:22 -0700

    commit

---
+++ day 06/classwork/mine.py
@@ -0,0 +1,27 @@
+#komentars pitonshi viyenebt radgan kodis dawerisas gavarkviot tu ra sad davweret da ar avibnat
+#snake case _ qvedatire romelic gamoiyeneba or sityviani cvladistvis
+
+# 1. first_user = "Lika"
+# 2nduser = "Giorgi"
+# print(first user)
+
+user1st = "Lika"
+user2nd = "Giorgi"
+print(user1st)
+
+# 2. first name = Data
+# last % name = "random"
+ 
+first_name = "Data"
+last_name = "random"
+
+ 
+# Num1 = 20
+# num2 = 30
+#  print(num1 + num2)
+#es kodi gamoitans erors radgan printshi num1 weria patara asoti xolo cvladi gviweria didi asoti
+Num1 = 20
+num2 = 30
+print(Num1 + num2)
+
+
---
+++ day 08/classwork/mine.py
@@ -0,0 +1,5 @@
+user_name = input("Enter your name: ")
+user_surname = input("Enter your surname: saba")
+user_age = input ("Enter your age: ")
+
+print(f"user name is {user_name} the surname is {user_surname} and age is {user_age}")
