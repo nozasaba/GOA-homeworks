702dbd81e20eaad1f506d8a8499d1cf10309fdee
Saba Nozadze <nozadzesaba80@gmail.com>
2025-05-17 02:41:31 -0700

    commit

---
+++ day 10/hw/mine.py
@@ -0,0 +1,67 @@
+# 2) კომენტარის სახით დაწერეთ თუ რა დანიშნულება აქვს type() ფუნქციას და რატომაა მისი გამოყენება მოხერხებული.
+
+# 3) ჩამოწერეთ ყველა ის ფუნქცია რომელიც “Data Conversion”-ს მოიცავს. გატესტეთ თითოეული ყველა ნასწავლ მონაცემთა ტიპზე.
+
+# 4) ცვლადებში შეინახეთ თქვენი სახელი,  გვარი და ასაკი (Integer) . ტერმინალში გამოიტანეთ წინადადება - რომელსაც კონკატინაციის შედეგად მიიღებთ. 
+# ვიცით რომ String-სა და Integer-ს შორის მათემატიკურ ოპერაციას ვერ შევასრულებთ, შესაბამისად დაწერეთ ისეთი კოდი, რომ Error-ების გარეშე გაეშვას ტერმინალში.
+
+# 5) მომხმარებელს შემოატანინეთ 5 რიცხვი. დაწერეთ პროგრამა, რომელიც გამოთვლის და დაბეჭდავს ამ რიცხვების საშუალო არითმეტიკულს. 
+
+#  (საშუალო არითმეტიკული - რიცხვთა ჯამის განაყოფი რიცხვების რაოდენობაზე)
+
+# 6) მომხმარებელს შემოატანინეთ ტემპერატურა ცელსიუსში. დაწერეთ პროგრამა, 
+# რომელიც მომხმარებლის მიერ შემოტანილ ტემპერატურას გადაიყვანს ფარენგეიტში და დაბეჭდეთ საბოლოო შედეგი. (მოიძიეთ ფორმულა ინტერნეტში)
+
+# 7) მეხუთე დავალების მსგავსად, შემოატანინეთ მომხმარებელს ტემპერატურა, თუმცა ამჯერად - ფარენგეიტში. შემდეგ კი დაწერეთ პროგრამა, 
+# რომელიც შემოტანილ ტემპერატურას ცელსიუსში გადაიყვანს და საბოლოოდ დაბეჭდეთ შედეგი.
+
+
+#type() tags gamoaq ra klasisaa tagi magalitad Type("10") gamoitans class-string
+
+#bool
+#str
+#int
+#float
+
+a = 34  boolean
+b = 21
+print (a > b)
+
+print("string")  string,
+a = 1  integer,
+print(a),
+
+a = 1.1 float,
+print(a)
+
+
+name = "saba"
+lastname = "Nozadze"
+age = 17
+
+print (f"my name is {name} {lastname} my age is {age}")
+
+# real_number = 3,
+# result = string_number + str(real_number),
+# print(result)
+
+string_number = "5"
+real_number = 3
+result = int(string_number) + real_number
+print( result)
+
+
+num1 = input (" enter any number: ")
+num2 = input (" enter any number: ")
+num2 = input (" enter any number: ")
+num3 = input (" enter any number: ")
+num4 = input (" enter any number: ")
+num5 = input (" enter any number: ")
+
+
+fahrenheit = (celsius * 9/5) + 32,
+print(fahrenheit)
+
+fahrenheit = float(input("enter temperature in farenheit: ")),
+celsius = (fahrenheit - 32) * 5 / 9,
+print(celsius)
