8af5783c12fa4c86687eb80af30a112a26ab9c03
Saba Nozadze <nozadzesaba80@gmail.com>
2025-05-24 11:37:06 -0700

    commit

--- day 12/hw/mine.py
+++ day 12/hw/mine.py
@@ -1,1 +1,51 @@
-# 
+# 2) გაიხსენეთ ყველა ის შედარების ოპერატორი , რომელიც ვისწავლეთ.
+# == > < >= <= !=
+#  გააკეთეთ თითოეულ შედარების ოპერატორზე 3-3 მაგალითი
+num1 = 10
+num2 = 5
+print(num1 > num2)
+print(num1 > num2)
+print(num1 > num2)
+
+print(num1 < num2)
+print(num1 < num2)
+print(num1 < num2)
+
+print(num1 == num2)
+print(num1 == num2)
+print(num1 == num2)
+
+print(num1 <= num2)
+print(num1 <= num2)
+print(num1 <= num2)
+
+print(num1 >= num2)
+print(num1 >= num2)
+print(num1 >= num2)
+
+print(num1 != num2)
+print(num1 != num2)
+print(num1 != num2)
+
+
+
+
+# 4) მომხმარებელს შემოატანინეთ სიმაღლე სანტიმეტრებში. ცალკე ცვლადში კი შეინახეთ თქვენი სიმაღლე.
+# დაადგინეთ მომხმარებელი არის თუ არა თქვენზე მაღალი.
+my_height= 175
+user_height = float(input("enter your height: "))
+print(user_height > my_height)
+
+
+
+# 5) დეტალურად ახსენით თუ რატომ გამოიტანს მოცემული კოდი False-ს ტერმინალში:
+
+# num1 = "14"
+# num2 = 14
+# print(num1 == num2)  int da stringis shedareba ar sheidzleba da magito false iqneba resoul
+
+# 6) ცვლადში შეინახეთ თქვენი გვარი, შემდეგ კი მომხმარებელს შემოატანინეთ მისი გვარი. 
+# დაადგინეთ თქვენი და მომხმარებლის გვარები ემთხვევა თუ არა ერთმანეთს.
+my_surname = "Nozadze"
+user_surname = input("enter your surname: ")
+print( my_surname == user_surname)
---
+++ day 14/cw/mine.py
@@ -0,0 +1,37 @@
+# 1) ჩამოწერეთ ყველა შესაძლო ლოგიკური ოპერაციის შედეგი.
+
+# 2) ტერმინალში დანახვის გარეშე დაწერეთ თქვენი აზრით რას გამოიტანს ეს ლოგიკური ოპერაცია:
+# True and False or False and False or True
+
+# 3) დაწერეთ პროგრამა სახელწოდებით soil_moisture, სადაც მცენარე მხოლოდ მაშინ უნდა მოირწყას, როდესაც ნიადაგის ნესტიანობა იქნება 100-ზე მეტი.
+
+# 4) გააკეთეთ თითო მაგალითი ყველა ლოგიკური ოპერაციის კომბინაციაზე, თუმცა True და False-ის მაგივრად ორ რიცხვს შორის შედარების ოპერაციები ჩასვით.
+
+
+
+# print (True and falls) print (true and True) print (True or True) print (True or Falls)
+# print (True or Falls)  print (Falls and Falls) print (Falls or Falls) print (True or falls)
+# True and False or False and False or True 
+#     Falls      or        Falls    or True   gamoitans True-s radgan or-ze Trues aq upiratesoba
+
+soil_moisture = 100
+num1 = 102
+
+print (soil_moisture < num1)
+
+num1 = 12
+num2 = 13
+
+print(num1 > num2 or num2 > num1)
+print(num2 > num1 or num1 > num2)
+print(num1 > num2 or num1 < num2)
+print(num2 > num1 or num2 < num1)
+print(num1 > num2 and num1 < num2)
+print(num2 < num1 and num2 >num1)
+print(num1 < num2 and num2 < num1)
+print(num2 > num1 and num1 > num2)
+
+
+
+
+
