94645b62a8ab1558a91c97c46f1634bd46bfba74
Saba Nozadze <nozadzesaba80@gmail.com>
2025-08-02 08:43:20 -0700

    commit

---
+++ day 43/cw/main.py
@@ -0,0 +1,24 @@
+# 1) დაწერეთ ფუნქცია, რომელიც მიიღებს რიცხვს და დააბრუნებს მის ორმაგ მნიშვნელობას.
+
+# 2) შექმენით ფუნქცია, რომელსაც არგუმენტად გადასცემთ 3 რიცხვს. ამ ფუნქციამ პასუხად უნდა დააბრუნოს პირველი ორი რიცხვის ჯამი გაყოფილი მესამე რიცხვზე.
+
+# 3) შექმენით ფუნქცია, რომელიც არგუმენტად მიიღებს სიას და ტერმინალში დაბეჭდავს სიის ყველა ელემენტს.
+
+# 4)თქვენი სიტყვებით ახსენით თუ რა განსხვავებაა print-სა და return keyword-ებს შორის.
+
+# მოცემული დავალებების შესასრულებლად გამოიყენეთ return keyword.
+
+
+def double_number(num):
+    return num * 2
+
+def sum_divide(a,b,c):
+    return (a + b) / c
+
+def print_list_elements(my_list):
+    for element in my_list:
+        print(element)
+
+
+        # print gamoiyeneba imistvisrom terminalshi gamoitanos informacia
+        #return gamoiyeneb  funqviidan mnishvnelobis dasabuneblad
